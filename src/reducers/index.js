005ff97162ae2cb28919fc1c46048bc16f8df8da
Ming Liu <mliu@macpro.home>
2019-05-13 15:24:52 -0400

    completion

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,8 @@ const initialState = {
   count: 0
 }
 
+
+
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
 // of our redux store, along with an action created
@@ -12,9 +14,19 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      console.log(action);
+      console.log(state.count);
+      return {
+        ...state,
+        count :++state.count
+      }
     case DECREMENT:
-    // Fill in the body of this case
+      console.log(action);
+      console.log(state.count);
+        return {
+          ...state,
+          count : --state.count
+        }
     default:
       return state;
   }
